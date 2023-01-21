55c0e368d022bef621604e6c66c642f82af0385d
shotaro27 <shotaro27@users.noreply.github.com>
2023-01-22 05:26:10 +0900

    プロジェクト ファイルを追加します。

---
+++ RedSheetApp1/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedSheetApp1.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+			string dateTime = DateTime.Now.ToString("d", new CultureInfo("ja"));
+			ViewData["TimeStamp"] = dateTime;
+		}
+	}
+}
