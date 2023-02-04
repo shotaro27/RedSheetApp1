fcd28fd98b8ec1c9473d20c86834468f269ec368
shotaro27 <shotaro27@users.noreply.github.com>
2023-02-04 15:28:39 +0900

    出題を一文につき一つにする

--- RedSheetApp1/Pages/Questions/RedSheet.cshtml.cs
+++ RedSheetApp1/Pages/Questions/RedSheet.cshtml.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RedSheetApp1.Data;
 using RedSheetApp1.Models;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace RedSheetApp1.Pages.Questions
 {
@@ -44,18 +46,81 @@ namespace RedSheetApp1.Pages.Questions
 
             Keywords = await _context.Keywords.Where(k => k.QuestionID == id).ToListAsync();
 
-            if (Question == null)
+            if (Question == null || Keywords == null)
             {
                 return NotFound();
             }
+
             QString = HttpUtility.HtmlEncode(Question.Text);
 
-            foreach (var keyword in Keywords.Select(k => k.Word))
+            var wrongKeywords = Keywords.Where(k => !(k.RightOrWrong ?? false)).ToList();
+
+            if (wrongKeywords.Count == 0)
             {
-                QString = QString.Replace(keyword, $"<span class='redsheet-parent'><a class='redsheet' style='width: {keyword.Length}em;'></a>{keyword}</span>");
+                return Redirect("./Index");
             }
 
+            var renderText = new StringBuilder();
+            var rand = new Random();
+            var split = QString.Split('。');
+
+            foreach (var sentence in split)
+            {
+                if (sentence.Length == 0)
+                {
+                    renderText.Append(sentence);
+                    continue;
+                }
+                var mc = Regex.Matches(sentence, string.Join("|", wrongKeywords.Select(k => Regex.Escape(k.Word))));
+                if (mc.Count == 0)
+                {
+                    renderText.Append($"{sentence}。");
+                }
+                else
+                {
+                    var m = mc[rand.Next(0, mc.Count)];
+                    var keyword = Keywords.FirstOrDefault(k => k.Word == m.Value);
+                    if (keyword == null)
+                    {
+                        renderText.Append($"{sentence}。");
+                    }
+                    else
+                    {
+                        var appendText = $"<span class='redsheet-parent' keyword-id='{keyword.KeywordsID}'>"
+                        + $"<div class='redsheet-option' role='group'>"
+                        + $"<input type='radio' class='btn-check' name='right-or-wrong-{keyword.KeywordsID}' id='right-{keyword.KeywordsID}' autocomplete='off' value='1'>"
+                        + $"<label class='redsheet-right' for='right-{keyword.KeywordsID}'> ○ </label>"
+                        + $"<input type='radio' class='btn-check' name='right-or-wrong-{keyword.KeywordsID}' id='wrong-{keyword.KeywordsID}' autocomplete='off' value='0'>"
+                        + $"<label class='redsheet-wrong' for='wrong-{keyword.KeywordsID}'> × </label>"
+                        + "</div>"
+                        + $"<a class='redsheet'>{keyword.Word}</a></span>";
+                        renderText.Append($"{sentence[..m.Index]}{appendText}{sentence[(m.Index + m.Length)..]}。");
+                    }
+                }
+            }
+
+            QString = renderText.ToString();
+
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string msg)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var sets = msg.Split("|");
+            foreach (var set in sets.Take(sets.Length - 1))
+            {
+                var split = set.Split(",");
+                var Keyword = await _context.Keywords.FindAsync(int.Parse(split[0]));
+                Keyword.RightOrWrong = split[1] == "1";
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
