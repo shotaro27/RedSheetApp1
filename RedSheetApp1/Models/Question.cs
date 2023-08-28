8b401941011794bb9ad3466a6c3e73a2db6c3faf
shotaro27 <shotaro27@users.noreply.github.com>
2023-08-28 23:36:24 +0900

    ChatGPTを用いたキーワード抽出

--- RedSheetApp1/Models/Question.cs
+++ RedSheetApp1/Models/Question.cs
@@ -22,10 +22,43 @@ namespace RedSheetApp1.Models
         static readonly string endpoint = "https://redsheet.cognitiveservices.azure.com/";
         static readonly string subscriptionKey = "ff767535093e42d68e8ea7b016ae52bd";
 
+        static readonly string ChatGPTKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY", EnvironmentVariableTarget.User);
+
         public KeyPhraseCollection ExtractKeywords()
         {
             var textClient = AuthenticateTextAnalytics(endpoint, subscriptionKey);
             return KeyPhraseExtract(textClient, Text);
+        }
+
+        internal async Task<string> GetKeywordsFromChatGPTAsync()
+        {
+            var api = new OpenAI_API.OpenAIAPI(ChatGPTKey);
+            var chat = api.Chat.CreateConversation();
+
+            var text = @$"# Imperative Statement:
+You are a school teacher.
+Based on the following constraints and input sentences, mark the key words and phrases in the text.
+
+# Following Constraints:
+- Mark possible questions in the form of fill-in-the-blanks in the reference book.
+- Do not leave out important key words.
+- Mark as many words as possible.
+- Mark in the format ""<span class=""keyword-wrong"">keywords</span>"".
+- Output in HTML format.
+- Do not use p tags.
+
+# Input Sentences:
+{Text}
+
+# Output Sentences:
+";
+            // ChatGPTに質問
+            chat.AppendUserInput(text);
+
+            // ChatGPTの回答
+            string response = await chat.GetResponseFromChatbotAsync();
+            Console.WriteLine(response);
+            return response;
         }
 
         public static TextAnalyticsClient AuthenticateTextAnalytics(string endpoint, string key)
