0a30ec6460c8089019b6ae68f0fddf90e3bc0def
shotaro27 <shotaro27@users.noreply.github.com>
2023-08-31 08:20:37 +0900

    index調整

--- RedSheetApp1/Pages/Index.cshtml.cs
+++ RedSheetApp1/Pages/Index.cshtml.cs
@@ -6,22 +6,33 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using RedSheetApp1.Data;
+using RedSheetApp1.Models;
+using RedSheetApp1.Pages.Questions;
+using Microsoft.EntityFrameworkCore;
 
 namespace RedSheetApp1.Pages
 {
-	public class IndexModel : PageModel
-	{
-		private readonly ILogger<IndexModel> _logger;
+    public class IndexModel : PageModel
+    {
+        private readonly RedSheetApp1Context _context;
 
-		public IndexModel(ILogger<IndexModel> logger)
-		{
-			_logger = logger;
-		}
+        public IndexModel(RedSheetApp1Context context)
+        {
+            _context = context;
+        }
 
-		public void OnGet()
-		{
-			string dateTime = DateTime.Now.ToString("d", new CultureInfo("ja"));
-			ViewData["TimeStamp"] = dateTime;
-		}
-	}
+        public IList<Question> Question { get; set; }
+        public IList<QuestionSet> QuestionSets { get; set; }
+        public IList<Keywords> Keywords { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Question = await _context.Question.ToListAsync();
+            Keywords = await _context.Keywords.ToListAsync();
+            QuestionSets = Question.Select(q => new QuestionSet(q, Keywords)).OrderByDescending(s => s.Question.UpdateDate).ToList();
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("ja"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
 }
